b2b8fc65ec6162b8fe91010663ab925144846fe2
Olivier Vincent <ovinc.py@gmail.com>
2023-01-12 17:16:48 +0100

    Move Zone class to separate module and inherits from TransformParameter

--- imgseries/grey_level.py
+++ imgseries/grey_level.py
@@ -4,118 +4,14 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 # Non-standard modules
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import imgbasics
-from imgbasics.cropping import _cropzone_draw
 
 # Local imports
 from .general import ImgSeries, Analysis
-
-
-# ==================== Class to manage zones of interest =====================
-
-
-class Zones:
-    """Class to store and manage areas of interest on series of images."""
-
-    def __init__(self, img_series):
-        """Init Zones object.
-
-        Parameters
-        ----------
-        - img_series: object of an image series class (e.g. GreyLevel)
-        """
-        self.img_series = img_series  # ImgSeries object on which to define zones
-        self.data = {}  # dict {'zone 1": (x, y, w, h), 'zone 2': ... etc.}
-
-    def define(self, n=1, num=0, draggable=False):
-        """Interactively define n zones image.
-
-        Parameters
-        ----------
-        - n: number of zones to analyze (default 1)
-
-        - num: image ('num' id) on which to select crop zones. Note that
-          this number can be different from the name written in the image
-          filename, because num always starts at 0 in the first folder.
-
-        - draggable: use draggable rectangle from drapo to define crop zones
-          instead of clicking to define opposite rectangle corners.
-
-        Output
-        ------
-        None, but stores in self.data a dict with every cropzone used during
-        the analysis, with:
-        Keys: 'zone 1', 'zone 2', etc.
-        Values: tuples (x, y, width, height)
-        """
-        img = self.img_series.read(num=num)
-
-        if img.ndim > 2:
-            kwargs = {}
-        else:
-            kwargs = {'cmap': 'gray'}
-
-        fig, ax = plt.subplots()
-        ax.imshow(img, **kwargs)
-        ax.set_title('All zones defined so far')
-
-        zones = {}
-
-        for k in range(1, n + 1):
-
-            msg = f'Select zone {k} / {n}'
-
-            _, cropzone = imgbasics.imcrop(img,
-                                           message=msg,
-                                           draggable=draggable,
-                                           **kwargs)
-
-            name = f'zone {k}'
-            zones[name] = cropzone
-            _cropzone_draw(ax, cropzone, c='b')
-
-        plt.close(fig)
-
-        self.data = zones
-
-    def show(self, num=0, **kwargs):
-        """show the defined zones on image (image id num if specified)
-
-        Parameters
-        ----------
-        - num: id number of image on which to show the zones (default first one).
-        - **kwargs: matplotlib keyword arguments for ax.imshow()
-        (note: cmap is grey by default)
-        """
-        ax = self.img_series.show(num, **kwargs)
-        ax.set_title(f'Analysis Zones (img #{num})')
-
-        for zone in self.data.values():
-            _cropzone_draw(ax, zone, c='r')
-
-        return ax
-
-    def load(self, filename=None):
-        """Load zones data from .json file and put it in zones.data.
-
-        If filename is not specified, use default filenames.
-
-        If filename is specified, it must be an str without the extension, e.g.
-        filename='Test' will load from Test.json.
-        """
-        self.data = self.img_series.load_metadata(filename=filename)['zones']
-
-    @property
-    def is_empty(self):
-        status = True if self.data == {} else False
-        return status
-
-
-# ================================ Main Class ================================
+from .image_transform import Zones
 
 
 class GreyLevel(ImgSeries, Analysis):
